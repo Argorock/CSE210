8f2c74402a69504c986185ded5fc1bbdbf051045
Daniel Wahlquist <triforce63@icloud.com>
2025-01-13 22:17:10 -0700

    prep #1 and #2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("what is your grade percentage? ");
+        string gradePercentage = Console.ReadLine();
+        int gradePercent = int.Parse(gradePercentage);
+        string letter = "";
+        if (gradePercent >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercent >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercent >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercent >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"{letter}");
+        if (gradePercent >= 70)
+        {
+            Console.WriteLine("You Passed, nice job! ");
+        }
+        else
+        {
+           Console.WriteLine("Try again next time"); 
+        }
+        
+
+
     }
+
 }
