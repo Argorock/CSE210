8b7dfa3f9810c6477de1827426f7363ef66d67e9
Daniel Wahlquist <triforce63@icloud.com>
2025-02-05 23:27:55 -0700

    commit

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -18,6 +18,19 @@ class Program
 
     // rect.
 
-    
+    Fraction frac1 = new Fraction();
+    Fraction frac2 = new Fraction(6);
+    Fraction frac3 = new Fraction(6, 7);
+
+    frac1.SetNumerator(3);
+    frac1.SetDenominator(4);
+
+    Console.WriteLine($"Fraction 1: {frac1.GetFractionString()} = {frac1.GetDecimalValue()}");
+    Console.WriteLine($"Fraction 2: {frac2.GetFractionString()} = {frac2.GetDecimalValue()}");
+    Console.WriteLine($"Fraction 3: {frac3.GetFractionString()} = {frac3.GetDecimalValue()}");
+
+    frac3.SetNumerator(1);
+    frac3.SetDenominator(3);
+    Console.WriteLine($"Modified Fraction 3: {frac3.GetFractionString()} = {frac3.GetDecimalValue()}");
     }
 }
